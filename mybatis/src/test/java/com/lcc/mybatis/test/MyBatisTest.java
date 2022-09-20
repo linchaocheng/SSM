031a8e7bb6734c60761840bc765b3befff953f12
linchaocheng <104829206+linchaocheng@users.noreply.github.com>
2022-09-20 17:39:39 +0800

    Update MyBatisTest.java

--- mybatis/src/test/java/com/lcc/mybatis/test/MyBatisTest.java
+++ mybatis/src/test/java/com/lcc/mybatis/test/MyBatisTest.java
@@ -20,7 +20,7 @@ public class MyBatisTest {
         SqlSession sqlSession = sqlSessionFactory.openSession();
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
         int result = userMapper.saveUser();
-        System.out.println(result+"======2");
+        System.out.println(result+"======3");
         sqlSession.commit();
         sqlSession.close();
     }
