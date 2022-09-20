ce6ec1001bf06268a3a574cdc9c716f68e04db90
lcc <lcc@git.com>
2022-09-20 17:31:47 +0800

    init

--- mybatis/src/test/java/com/lcc/mybatis/test/MyBatisTest.java
+++ mybatis/src/test/java/com/lcc/mybatis/test/MyBatisTest.java
@@ -20,7 +20,7 @@ public class MyBatisTest {
         SqlSession sqlSession = sqlSessionFactory.openSession();
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
         int result = userMapper.saveUser();
-        System.out.println(result+"======");
+        System.out.println(result+"======1");
         sqlSession.commit();
         sqlSession.close();
     }
