15605747802a310ac0186107a5f762f07f9783f6
lcc <lcc@git.com>
2022-09-20 16:45:59 +0800

    first2

---
+++ mybatis/src/test/java/com/lcc/mybatis/test/MyBatisTest.java
@@ -0,0 +1,28 @@
+package com.lcc.mybatis.test;
+
+import com.lcc.mybatis.mapper.UserMapper;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import org.apache.ibatis.io.Resources;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MyBatisTest {
+    @Test
+    public void testInsert() throws IOException {
+        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        int result = userMapper.saveUser();
+        System.out.println();
+        System.out.println(result+"=======");
+        sqlSession.commit();
+        sqlSession.close();
+    }
+}
